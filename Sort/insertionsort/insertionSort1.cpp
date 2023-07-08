494e7622505dcbe4e12efab703db12367f55b88f
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-07-09 00:48:51 +0800

    add : insertionSort and selectoinSort

--- Sort/insertionsort/insertionSort1.cpp
+++ Sort/insertionsort/insertionSort1.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
 #include <random>
-
 using namespace std;
-// #define DEBUG
+
 /***
- * @brief 选择排序 Selection Sort
+ * @brief 삽입 정렬 Insertion Sort
  * @param vector<int> &A : a vector of integers
  * @return void
  * @remark A를 오름차순으로 정렬한다.
  * @remark 만약 중복된 값이 있을 경우, 그 중 더 뒤에 위치한 값을 선택한다.
 */
-void selectionSort(vector<int> & A){
-    for(int i=0; i<A.size();++i){
-        int minIndex = i;
-        for(int j=i+1; j<A.size();++j){
-            if(A[minIndex] > A[j]){
-                minIndex = j;
-            }
+void insertionSort(vector<int> & A){
+    for(int i=0; i<A.size(); ++i){
+        int j = i;
+        while(j>0 && A[j-1] > A[j]){
+            swap(A[j-1],A[j]);
+            j--;
         }
-        swap(A[i],A[minIndex]);
-        #ifdef DEBUG
-            cout<<"Index :" <<i<< " value : "<<A[i]<<endl;
-        #endif  
     }
 }
 int main(){
-
     //Create a random vector
     std::random_device rd;
     std::mt19937 eng(rd());
@@ -40,7 +33,7 @@ int main(){
     }
     printf("\n");
     //sort
-    selectionSort(A);
+    insertionSort(A);
     //print
     cout<<endl<<"AFTER SORTING"<<endl;
     for(int i=0; i<A.size();i++){
