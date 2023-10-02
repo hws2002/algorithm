9ce6a85c680b8e27bffabe3d1249dbd090148d14
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-10-02 16:11:03 +0900

    solve : 1 (leetcode)

---
+++ leetcode/Problem_List/1-Two_Sum/brute_force.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target){
+    for (int i = 0; i < nums.size(); i++ ){
+        for (int j = i+1 ; j < nums.size(); j++){
+            if(nums[i] + nums[j] == target){
+                return {i,j};
+            }
+        }
+    }
+    return {};
+}
+
+int main() {
+    // input
+    vector<int> nums;
+    int target;
+    int n_nums;
+    cin>>n_nums;
+    for (int i=0 ; i< n_nums; i++){
+        int num;
+        cin>>num;
+        nums.push_back(num);
+    }
+    cin>>target;
+    
+    // solve
+    vector<int> res = twoSum(nums,target);
+    
+    // print
+    if (!res.empty()){
+        for (int i = 0; i<res.size(); i++){
+            cout<<res[i]<<" ";
+        }
+    } else{
+        cout<<"trivial";
+    }
+    return 0;
+}
