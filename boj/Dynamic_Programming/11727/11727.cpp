d18087c7b2b52734b63ea0d20134130a8f26ba58
hws2002 <hwooseok123@gmail.com>
2024-04-09 14:48:59 +0000

    solve : 11727(2xn 타일링2)

---
+++ boj/Dynamic_Programming/11727/11727.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define MOD 10007
+#define endl '\n'
+
+using namespace std;
+
+int RES[1001] = {0};
+
+int dp(const int & n){
+    if(n == 0 | n==1) return n;
+    if(n == 2) return 3;
+    if(RES[n]) return RES[n];
+    int res = (dp(n-1) + 2 * dp(n-2))%MOD;
+    RES[n] = res;
+    return res % MOD;
+}
+
+int main(){
+    //input
+    int n;
+    cin>>n;
+    
+    
+    //solve
+    cout<<dp(n) % MOD<<endl;    
+    return 0;
+}
