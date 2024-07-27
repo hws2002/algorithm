2e04a84c780b6b08f1241d5bc8edaccda7985418
hws2002 <hanys21@mails.tsinghua.edu.cn>
2024-07-27 14:35:45 +0900

    solve : SCPC2024 ROUND2 A dp

---
+++ SCPC/Round2/B/B.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+ll Answer;
+
+int main(int argc, char** argv)
+{
+	int T, test_case;
+
+	// freopen("input.txt", "r", stdin);
+
+	cin >> T;
+	for(test_case = 0; test_case  < T; test_case++)
+	{
+
+		Answer = 0;
+		/////////////////////////////////////////////////////////////////////////////////////////////
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		
+		// Print the answer to standard output(screen).
+		cout << "Case #" << test_case+1 << endl;
+		cout << Answer << endl;
+	}
+
+	return 0;//Your program should return 0 on normal termination.
+}
