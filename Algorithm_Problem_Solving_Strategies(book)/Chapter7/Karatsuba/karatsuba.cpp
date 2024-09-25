de7b7e53f00810fdd362e8746f6731a74cf70ff9
hws2002 <hwooseok123@gmail.com>
2024-09-25 14:02:03 +0000

    organize folders

---
+++ Algorithm_Problem_Solving_Strategies(book)/Chapter7/Karatsuba/karatsuba.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+#define MAX_LEN 300'000
+using namespace std;
+
+int main(){
+    char a[MAX_LEN+1];
+   	scanf("%s",a);
+	printf("%s \n", a);
+    char b[MAX_LEN+1];
+	scanf("%s",b);
+	printf("%s \n", b);
+    return 0;
+}
