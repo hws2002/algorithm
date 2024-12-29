45f289824a0e4762e806ae48423463e66e1bf6b0
hws2002 <hwooseok123@gmail.com>
2024-12-29 09:51:25 +0000

    solve : 14505(팰린드롬 개수 구하기(Small)) dp

---
+++ boj/CPP/Dynamic_Programming/14505/14505.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#define MOD 10'007
+#define MAX_N 1'000
+
+using namespace std;
+
+string S;
+int N;
+
+int cache[MAX_N][MAX_N] = {{0}};
+
+int dp(int s, int e){
+    if( s >= N || e < 0 || s > e) return 0;
+    if( e == s) return cache[s][e] = 1;
+    
+    int & ret = cache[s][e];
+    if(ret) return ret;
+    
+    if(S[s] == S[e])
+        return ret = (dp(s+1,e) + dp(s,e-1) + 1);
+    else 
+        return ret = (dp(s+1,e) + dp(s,e-1) - dp(s+1, e-1));
+        
+    return ret % MOD;
+}
+
+int main(){
+    cin>>S;
+    //base
+    N = S.size();
+    cout<<dp(0,N-1)<<endl;
+    return 0;
+}
