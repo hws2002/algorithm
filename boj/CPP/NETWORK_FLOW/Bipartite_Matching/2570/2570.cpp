3640206d5b4cbdc9d272524e82c20f0f3c38e11a
hws2002 <hwooseok123@gmail.com>
2025-01-12 14:09:52 +0000

    solve : 2570(비숍2) 이분매칭, grid graph

---
+++ boj/CPP/NETWORK_FLOW/Bipartite_Matching/2570/2570.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#define MAX_N 100
+#define MAX_LIDX 10000
+#define endl '\n'
+using namespace std;
+int N,M;
+vector< vector<int> > board;
+
+int coor2L[MAX_N][MAX_N] = {{-1}};
+int coor2R[MAX_N][MAX_N] = {{-1}};
+int lidx = 0; int ridx = 0;
+vector<int> adj[MAX_LIDX]; // adj[a] : a번째 left노드의 인접 right 노드들
+vector<int> aMatch;
+vector<int> bMatch;
+vector<bool> visited;
+
+bool isOccupiedL(int x, int y){
+    return coor2L[x][y] > -1;
+}
+
+bool isOccupiedR(int x, int y){
+    return coor2R[x][y] > -1;
+}
+
+bool placable(int x, int y){
+    if( 0 <= x && x < N && 0 <= y && y < N) return board[x][y];
+    return false;
+}
+
+
+bool dfs(int a){
+    if( visited[a] ) return false;
+    visited[a] = true;
+    for(const auto & b : adj[a]){
+        if( bMatch[b] == -1 || dfs( bMatch[b])){
+            aMatch[a] = b;
+            bMatch[b] = a;
+            return true;
+        }
+    }
+    return false;
+}
+
+int bipartiteMatch(int n, int m){
+    aMatch = vector<int> (n, -1);
+    bMatch = vector<int> (m, -1);
+    int ret = 0;
+    for(int i = 0; i < n; i++){
+        visited = vector<bool> (n , false);
+        if(dfs(i)) ret++;
+    }
+    return ret;
+}
+
+void solve(){
+    cin>>N;
+	cin>>M;
+    board = vector<vector<int>>(N, vector<int>(N,1));
+	//reset
+	for(int i = 0; i < N; i++){
+        fill(coor2L[i], coor2L[i] + N, -1);
+        fill(coor2R[i], coor2R[i] + N, -1);
+	}
+	
+	int r,c;
+    for(int i = 0 ; i < M; i++){
+        cin>>r>>c;
+		board[--r][--c] = 0;
+    }
+    
+    //create bipartite-graph
+    lidx = 0; ridx = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if( placable(i,j)){
+                if( !isOccupiedL(i,j)){ // create leftNode
+                    int x1 = i; int y1 = j;
+                    while( placable(x1+1,y1-1)){
+                        x1++; y1--;
+                        coor2L[x1][y1] = lidx;
+                    }
+                    coor2L[i][j] = lidx++;
+                }
+                
+                if( !isOccupiedR(i,j)){//create rightNode
+                    int x1 = i; int y1 = j;
+                    while( placable(x1+1, y1+1) ){
+                        x1++; y1++;
+                        coor2R[x1][y1] = ridx;
+                    }
+                    coor2R[i][j] = ridx++;
+                }
+                
+				//add edges
+                adj[coor2L[i][j]].push_back( coor2R[i][j] );
+				// printf("adj[%d].push_back(%d) \n", coor2L[i][j], coor2R[i][j]);
+            }
+        }
+    }
+    
+	// cout<<"lidx , ridx : "<<lidx<<","<<ridx<<endl;
+    //bipartiteMatch
+    cout<<bipartiteMatch(lidx, ridx)<<endl;
+	for(int i = 0; i < lidx; i++) adj[i].clear();
+}
+
+int main(){
+    solve();
+    return 0;
+}
